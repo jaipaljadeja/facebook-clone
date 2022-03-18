e67241d175cc37fc7d04fbbe7c193cd34cdde652
Jaipal Jadeja <jadejajaipal5@gmail.com>
2022-03-18 12:10:05 +0530

    First commit

---
+++ components/Header.js
@@ -0,0 +1,61 @@
+import Image from 'next/image';
+import {
+  BellIcon,
+  ChatIcon,
+  ChevronDownIcon,
+  HomeIcon,
+  UserGroupIcon,
+  ViewGridIcon,
+} from '@heroicons/react/solid';
+import {
+  FlagIcon,
+  PlayIcon,
+  SearchIcon,
+  ShoppingCartIcon,
+} from '@heroicons/react/outline';
+import HeaderIcon from './HeaderIcon';
+
+function Header() {
+  return (
+    <div className="flex sticky top-0 z-50 bg-white items-center p-2 lg:px-5 shadow-md">
+      {/* Left */}
+      <div className="flex items-center">
+        <Image
+          src="https://links.papareact.com/5me"
+          width={40}
+          height={40}
+          layout="fixed"
+        />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <SearchIcon className="h-6 text-gray-600" />
+          <input
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            type="text"
+            placeholder="Search Facebook"
+          />
+        </div>
+      </div>
+      {/* Centre */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon active={true} Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
+      {/* RIght */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        {/* Profile Pic */}
+        <p className="font-semibold whitespace-nowrap pr-3">Jaipal Jadeja</p>
+        <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" />
+        <BellIcon className="icon" />
+        <ChevronDownIcon className="icon" />
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  images: {
+    domains: [
+      'links.papareact.com',
+      'platform-lookaside-fbsfx.com',
+      'firebasestorage.googleapis.com',
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
